d96ff0aa11713f53cf835c561cdf0dd1abac3c1c
KyleD <kvd101@francis.edu>
2015-10-05 14:01:50 -0400

    Added dynamic route

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+# static routes
 @app.route("/")
 @app.route("/hello")
 def hello_world():
 	return "Hello SFU World!"
-if __name__ == "__main__":
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+if __name__=="__main__":
 	app.run()
